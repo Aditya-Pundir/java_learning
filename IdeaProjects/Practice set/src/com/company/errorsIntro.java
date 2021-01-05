b69e79eb9faf7b74f9670ce992be7c0daae56d07
Aditya Pundir <68149659+Aditya-Pundir@users.noreply.github.com>
2021-01-05 13:40:08 +0530

    Add files via upload

---
+++ IdeaProjects/Practice set/src/com/company/errorsIntro.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class errorsIntro {
+    public static void main(String[] args) {
+//        There are 3 types of errors in Java:
+//                1. Syntax Error  --> Error in Syntax.
+//                2. Logical Error --> Error in the Logic in Backend of the program.
+//                3. Runtime Error --> Error while executing the program.
+        
+    }
+}
